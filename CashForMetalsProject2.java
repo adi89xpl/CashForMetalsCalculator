be057b881c9dce5beb4230ecff96a7680d1ef96b
Adi <adi89xpl@yahoo.com>
2012-11-23 20:45:33 -0600

    Initial Commit and Version

---
+++ CashForMetalsProject2.java
@@ -0,0 +1,8 @@
+import GUIBase.GUIModel;
+
+public class CashForMetalsProject2 {
+    public static void main(String[] args) {
+        GUIModel Main = new GUIModel();
+        Main.DisplayFrame();
+    }
+}
