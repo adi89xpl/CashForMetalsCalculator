253d6cc5061bc69c8be9152b20cfa72a0d7af455
Adi <adi89xpl@yahoo.com>
2012-11-23 21:42:20 -0600

    Fixing the Repo"

--- CashForMetalsProject2.java
+++
@@ -1,8 +0,0 @@
-import GUIBase.GUIModel;
-
-public class CashForMetalsProject2 {
-    public static void main(String[] args) {
-        GUIModel Main = new GUIModel();
-        Main.DisplayFrame();
-    }
-}
